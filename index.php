dd1cc07dc4f2e3df7f72346b6a955fb82831db90
TheFeynman137 <74338022+TheFeynman137@users.noreply.github.com>
2020-11-16 20:53:04 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php echo file_get_contents("html/header.html"); ?>
 <?php echo file_get_contents("html/body.html"); ?>
 <?php echo file_get_contents("html/footer.html"); ?>
-<p></p><p></p><p></p><p></p><p></p><p></p>
 <p>The End <?php echo date("Y"); ?></p> 
